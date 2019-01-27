ac099f7d30e94c0b80c7194630027d3981ea06bc
Olexyy <olexyy.mails@gmail.com>
2019-01-27 08:57:04 +0200

    added

---
+++ lesson3.py
@@ -0,0 +1,13 @@
+x = 13
+if x < 20:
+    print("you are old")
+
+x = 25
+if x > 20:
+    print("you are old")
+    print("what are you doing here")
+    print("why are you not in home")
+
+age = 10
+if age > 10:
+    print("you are old for my jokes!")
