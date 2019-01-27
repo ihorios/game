5ced252bebaa6059e8a3320b61c38940dfa853ba
Olexyy <olexyy.mails@gmail.com>
2019-01-27 09:47:51 +0200

    style changes

--- lesson3.py
+++ lesson3.py
@@ -6,8 +6,41 @@ x = 25
 if x > 20:
     print("you are old")
     print("what are you doing here")
-    print("why are you not in home")
 
-age = 10
-if age > 10:
+x = 10
+if x >= 10:
     print("you are old for my jokes!")
+
+x = 10
+if x == 10:
+    print("you are old for my jokes!")
+
+print("Хочете я вам розкажу брудний жарт ")
+fred = 12
+if fred == 12:
+    print("Свинья шлепнулась в грязь!")
+else:
+    print("Тсс! Это секрет.")
+
+print("Хочете я вам розкажу брудний жарт ")
+fred = 8
+if fred == 12:
+    print("Свинья шлепнулась в грязь!")
+else:
+    print("Тсс! Это секрет.")
+
+age = 12
+if age == 10:
+    print("Что выйдет, если клюква наденет штаны?")
+    print("Брюква!")
+elif age == 11:
+    print("Что сказала зеленая виноградина синей виноградине?")
+    print("Дыши! Дыши!")
+elif age == 12:
+    print("Что сказал 0 числу 8?")
+    print("Привет, ребята!")
+elif age == 13:
+    print("Что такое: на потолке сидит и хохочет?")
+    print("Муха-хохотуха!")
+else:
+    print("Что-что?")
